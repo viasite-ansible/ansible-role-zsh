71399b81854303b53a55c365869adde0b5151d46
Stanislav Popov <popstas@tagilcity.ru>
2017-05-14 01:34:31 +0500

    fix: make working command conditions on clean centos
    Centos container don't have `which` command,
    on Ubuntu `command -v` returns `command not exists` while playbook.
    
    So, I'm using both commands.
    
    Closes #8

--- tests/test_zsh.py
+++ tests/test_zsh.py
@@ -8,3 +8,9 @@ def test_bundles_when(File):
     c = File('/root/.zshrc').content
     assert 'test/in-list' in c
     assert 'test/not-in-list' not in c
+
+
+def test_bundles_command(File):
+    c = File('/root/.zshrc').content
+    assert 'test/command-exists' in c
+    assert 'test/command-not-exists' not in c
