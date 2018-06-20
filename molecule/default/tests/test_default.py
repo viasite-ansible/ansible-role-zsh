063bfe1560966942333b0fe2ed877c8aa49b3d04
Stanislav Popov <popstas@gmail.com>
2018-06-21 01:27:32 +0500

    test: fix test warnings

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,16 +1,17 @@
+import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_bundles_when(File):
-    c = File('/root/.zshrc').content
+def test_bundles_when(host):
+    c = host.file('/root/.zshrc').content
     assert 'test/in-list' in c
     assert 'test/not-in-list' not in c
 
 
-def test_bundles_command(File):
-    c = File('/root/.zshrc').content
+def test_bundles_command(host):
+    c = host.file('/root/.zshrc').content
     assert 'test/command-exists' in c
     assert 'test/command-not-exists' not in c
