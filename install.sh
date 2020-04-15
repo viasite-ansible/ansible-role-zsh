047bb1c0f2af63d770dd0c3b9188055921ca0a36
hybridadmin <46610714+hybridadmin@users.noreply.github.com>
2020-04-15 10:33:13 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,8 +20,5 @@ sudo ansible-galaxy install hybridadmin.fancy_console --force
 title "Download playbook to /tmp/zsh.yml"
 curl https://raw.githubusercontent.com/hybridadmin/ansible-role-fancy-console/master/playbook.yml > /tmp/zsh.yml
 
-title "Provision playbook for $(whoami)"
-sudo ansible-playbook -i "localhost," -c local /tmp/zsh.yml --extra-vars="zsh_user=$(whoami) dotfile_repo_url=DOT_FILE_REPO_URL"
-
-# Centos packages
-my_packages: ["bind-utils","keychain","whois","traceroute","zip","jq","curl","mysql-client","moreutils","sysstat","attr","lnav","percona-toolkit"]
+title "Provision playbook for current user: $(whoami)"
+sudo ansible-playbook -i "localhost," -c local /tmp/zsh.yml --extra-vars="zsh_user=$(whoami)"
